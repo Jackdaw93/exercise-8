17879f557fd781d61d5de37e9049ff13803e46fe
RohMad <rohmad37@ymail.com>
2020-06-11 15:08:16 +0700

    Exercise Js 8

---
+++ script.js
@@ -0,0 +1,72 @@
+let divisi = "Satpam"
+
+if(divisi === "HR") {
+    console.log("Melakukan rekrutmen untuk calon pegawai");
+}else if (divisi === "Programer" ) {
+    console.log("Melakukan pembuatan program");
+}else {
+    console.log("Tidak melakukan apapun");
+}
+
+/*
+let a = 5;
+let b = 7;
+
+if (a > b) {
+  console.log("Nilai a lebih besar dari b");
+} else if (a < b) {
+  console.log("Nilai a lebih kecil dari b");
+} else {
+  console.log("Nilai a sama dengan b");
+}
+*/
+/*
+let hari = 1;
+
+switch (hari) {
+  case 1:
+    console.log("Minggu");
+    break;
+  case 2:
+    console.log("Senin");
+    break;
+  case 3:
+    console.log("Selasa");
+    break;
+  case 4:
+    console.log("Rabu");
+    break;
+  case 5:
+    console.log("Kamis");
+    break;
+  case 6:
+    console.log("Jum'at");
+    break;
+  case 7:
+    console.log("Sabtu");
+    break;
+  default:
+    console.log("Bukan Hari");
+    break;
+}
+*/
+/*
+let teks = "UP";
+switch (teks) {
+  case "UP":
+    console.log("Karakter berjalan ke atas");
+    break;
+  case "RIGHT":
+    console.log("Karakter berjalan ke kanan");
+    break;
+  case "BOTTOM":
+    console.log("Karakter berjalan ke bawah");
+    break;
+  case "LEFT":
+    console.log("Karakter berjalan ke kiri");
+    break;
+  default:
+    console.log("karakter tidak melakukan apapun");
+    break;
+}
+*/
